07a8ba2b397df584ebce9da597f3abdc41bed02a
Timmy <timmy.pfau@netpy.de>
2022-12-28 10:48:41 +0100

    add backend-url servicname to terraform

--- local_helper/load_and_build_userservice.sh
+++ local_helper/load_and_build_userservice.sh
@@ -33,6 +33,7 @@ echo $GH_TOKEN | gh auth login --with-token
 # Admin Userservice
 gh release download --archive zip --clobber --repo https://github.com/LugsoIn2/cad-event-userservice.git --output cad-event-userservice.zip
 unzip -o cad-event-userservice.zip -d ./
+rm cad-event-userservice.zip
 cd cad-event-userservice-*
 npm install
 echo $VITE_API_ENVVAR > .env

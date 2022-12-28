321995c49b21baf0162ed07b023c665a6ef6ca2f
Timmy <timmy.pfau@netpy.de>
2022-12-28 10:11:23 +0100

    add backend-url servicname to terraform

--- local_helper/load_and_build_admin_ui.sh
+++ local_helper/load_and_build_admin_ui.sh
@@ -28,7 +28,7 @@ fi
 echo test1
 mkdir -p tmp_$CUSTOMER_NR\_admin_ui
 cd tmp_$CUSTOMER_NR\_admin_ui
-#echo $GH_TOKEN | gh auth login --with-token
+echo $GH_TOKEN | gh auth login --with-token
 echo test2
 # Admin UI Service
 gh release download --archive zip --clobber --repo https://github.com/LugsoIn2/cad-admin-ui-service.git --output cad-admin-ui-service.zip

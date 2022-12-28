f25d0d7f89d89991bf62e7ec86ccd80194b61e94
Timmy <timmy.pfau@netpy.de>
2022-12-28 11:00:15 +0100

    add backend-url servicname to terraform

--- local_helper/load_and_build_admin_ui.sh
+++ local_helper/load_and_build_admin_ui.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # input ./script.sh ghtoken customer_nr servicename https_enabled 
 if ! command -v gh &> /dev/null
 then
@@ -25,11 +25,9 @@ if [ $HTTPS_ENABLED -eq 1 ]; then
 else
     VITE_API_ENVVAR="VITE_API_ENDPOINT=http://$CUSTOMER_NR$SERVICENAME.aws.netpy.de:80"
 fi
-echo test1
 mkdir -p tmp_$CUSTOMER_NR\_admin_ui
 cd tmp_$CUSTOMER_NR\_admin_ui
 echo $GH_TOKEN | gh auth login --with-token
-echo test2
 # Admin UI Service
 gh release download --archive zip --clobber --repo https://github.com/LugsoIn2/cad-admin-ui-service.git --output cad-admin-ui-service.zip
 unzip -o cad-admin-ui-service.zip -d ./
@@ -41,5 +39,4 @@ npm run build --if-present
 cp -R dist ../
 cd ../
 rm -rf cad-admin-ui-service*
-
 #gh auth logout

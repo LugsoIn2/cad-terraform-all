013dbab5feaec6def7fbe319adbb3569696ae762
Hyerex <andreas.ly96@gmail.com>
2023-01-19 14:58:34 +0100

    Add tf for adminservicetable

---
+++ service_modules/tf_adminservicetable/prod/main.tf
@@ -0,0 +1,13 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = var.dbname
+  engine               = "mysql"
+  engine_version       = "8.0.28"
+  port                 = 3306
+  publicly_accessible  = true
+  instance_class       = "db.t2.micro"
+  username             = var.db_username
+  password             = var.db_password
+  skip_final_snapshot  = true
+
+}

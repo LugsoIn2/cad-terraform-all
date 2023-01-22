88fd926f32197335fc787ebc71ad51f2af54577f
Timmy <timmy.pfau@netpy.de>
2023-01-22 16:46:35 +0100

    fix uncomment changes

--- prod/main.tf
+++ prod/main.tf
@@ -3,19 +3,19 @@ locals {
  prod_admindb_name = "${terraform.workspace}_admintable"
 }
 
-# module "prod-userservice" {
-#   source = "./../service_modules/tf_userservice/prod/"
-#   bucket_name_userservice = "userservice"
-#   tags_userservice = {
-#       "Environment" = "Prod"
-#     }
-#   dist_assets_directory_userservice = "../local_helper/tmp_${terraform.workspace}_userservice/dist/assets"
-#   dist_directory_userservice = "../local_helper/tmp_${terraform.workspace}_userservice/dist"
-#   backend_servicename_userservice = "eventservice"
-#   access_key = var.access_key
-#   secret_key = var.secret_key
-#   gh_token = var.gh_token
-# }
+module "prod-userservice" {
+  source = "./../service_modules/tf_userservice/prod/"
+  bucket_name_userservice = "userservice"
+  tags_userservice = {
+      "Environment" = "Prod"
+    }
+  dist_assets_directory_userservice = "../local_helper/tmp_${terraform.workspace}_userservice/dist/assets"
+  dist_directory_userservice = "../local_helper/tmp_${terraform.workspace}_userservice/dist"
+  backend_servicename_userservice = "eventservice"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  gh_token = var.gh_token
+}
 
 module "prod-adm-ui-service" {
   source = "./../service_modules/tf_adm_ui_service/prod/"
@@ -31,14 +31,14 @@ module "prod-adm-ui-service" {
   gh_token = var.gh_token
 }
 
-# module "prod-eventservice" {
-#   source = "./../service_modules/tf_eventservice/prod/"
-#   release_name_and_namespace_k8s_eventservice = terraform.workspace
-#   allowed_hosts_eventservice = "${terraform.workspace}-eventservice.aws.netpy.de"
-#   ev_table_name = local.prod_ev_table_name
-#   access_key = var.access_key
-#   secret_key = var.secret_key
-# }
+module "prod-eventservice" {
+  source = "./../service_modules/tf_eventservice/prod/"
+  release_name_and_namespace_k8s_eventservice = terraform.workspace
+  allowed_hosts_eventservice = "${terraform.workspace}-eventservice.aws.netpy.de"
+  ev_table_name = local.prod_ev_table_name
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
 module "prod-adminservice" {
   source = "./../service_modules/tf_adminservice/prod/"
   release_name_and_namespace_k8s_adminservice = terraform.workspace
@@ -61,13 +61,13 @@ module "prod-tenants-database"{
     }
 }
 
-# module "prod-event-table" {
-#   source = "./../service_modules/tf_eventtableservice/prod"
-#   dbname = local.prod_ev_table_name
-#   tags_eventtable-database = {
-#       "Environment" = "Prod"
-#     }
-# }
+module "prod-event-table" {
+  source = "./../service_modules/tf_eventtableservice/prod"
+  dbname = local.prod_ev_table_name
+  tags_eventtable-database = {
+      "Environment" = "Prod"
+    }
+}
 
 module "prod-admin-table" {
   source = "./../service_modules/tf_admintableservice/prod"
@@ -75,8 +75,3 @@ module "prod-admin-table" {
   db_username = var.admindb_username
   db_password = var.admindb_password
 }
-
-
-# output "testforandi" {
-#   value = module.prod-admin-table.rds_address
-# }
